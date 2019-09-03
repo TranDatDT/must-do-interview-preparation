48af1c99d2a363d372fca27545ff0baab6a1d57a
Thanh Dat Tran <8818857+TranDatDT@users.noreply.github.com>
2019-09-03 10:57:40 +0700

    Update majority-element.py

--- majority-element.py
+++ majority-element.py
@@ -2,6 +2,8 @@
 Given an array A of N elements. Find the majority element in the array.
 A majority element in an array A of size N is an element
 that appears more than N/2 times in the array.
+
+Time complexity: O(n)
 """
 
 
